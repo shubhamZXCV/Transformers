4a05d3172c03fc953b4b8a8f33adc6241dd40e51
shubhamZXCV <shubhamlibra2005@gmail.com>
2025-09-04 23:28:47 +0530

    layerNorm , feedForward , MultiHeadAttention

--- model.py
+++ model.py
@@ -50,6 +50,108 @@ class PositonalEncoding(nn.Module):
     def forward(self ,x):
         x = x  + (self.pe[: , :x.shape[1], : ]).requires_grad(False) # (batch , seq_len , d_model)
         return self.dropout(x)
+    
+
+class LayerNormalisation(nn.Module):
+    def __init__(self , features : int , eps : float = 10**-6)->None:
+        super().__init__()
+        self.eps = eps
+        # beta and  gamma are trainable parameters
+        self.gamma = nn.Parameter(torch.ones(features))
+        self.beta = nn.Parameter(torch.ones(features))
+
+    def forward(self , x):
+        mean = x.mean(dim=-1 ,keepdim=True) # (batch , seq_len , 1)
+        std = x.std(dim=-1  , keepdim = True) # (batch , seq_len , 1)
+
+        return self.gamma * (x - mean)/(std + self.eps) + self.beta
+    
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self , d_model : int  , d_ff : int , dropout : float)->None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model , d_ff) # w1 and b1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff , d_model) # w2 and b2
+
+    def forward(self,x):
+        # (batch , seq_len , d_model) -> (batch , seq_len , d_ff) -> (batch  , seq_len , d_model)
+
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self , d_model : int , h : int , dropout : float)->None:
+        super().__init__()
+        self.d_model = d_model # embedding vector size
+        self.h = h #  number of heads
+
+         # make sure d_model is divisible by h
+        assert d_model % h ==0, "d_model is not divisible by h"
+
+        self.d_k = d_model // h # dimension of vector seen by each head
+        self.w_q = nn.Linear(d_model  , d_model  ,bias = False)
+         # Wq
+        self.w_k = nn.Linear(d_model , d_model  , bias = False) # Wk
+        self.w_v = nn.Linear(d_model  , d_model , bias = False) # Wv
+        self.w_o = nn.Linear(d_model , d_model , bias = False) # Wo
+        self.dropout = nn.Dropout(dropout)
+
+
+    def attention(query , key , value , mask , dropout : nn.Dropout):
+        d_k = query.shape[-1]
+
+        # (batch_size , h , seq_len , d_k) --> (batch_size , h  ,seq_len , seq_len)
+
+        attention_scores = (query @ key.transpose(-1,-2)) / math.sqrt(d_k)
+
+
+        if mask is not None:
+            # Write a very low value (indicating -inf) to the positions where mask == 0
+            attention_scores = attention_scores.masked_fill_(mask ==0 , -1e9)
+        attention_scores = attention_scores.softmax(dim=-1) # (batch , h, seq_len , seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        # (batch_size , h , seq_len , seq_len) --> (batch_size , h , seq_len , d_k)
+
+        # returning attention_scores also for visualisation
+        return (attention_scores @ value)  , attention_scores 
+
+
+    def forward(self , q , k , v  ,mask):
+        query = self.w_q(q) # (batch_size , seq_len , d_model)
+        key = self.w_k(k) # (batch_size , seq_len , d_model)
+        value = self.w_v(v) # (batch_size , seq_len , d_model)
+
+        # (batch_size , seq_len , d_model) -> (batch_size , seq_len , h , d_k) -> (batch_size , h , seq_len , d_model)
+        query = query.view(query.shape[0] , query.shape[1] , self.h , self.d_k).transpose(1,2)         
+        key = key.view(key.shape[0] , key.shape[1] , self.h , self.d_k).transpose(1,2)         
+        value = value.view(value.shape[0] , value.shape[1] , self.h , self.d_k).transpose(1,2)    
+
+        # calculate attention
+        x , self.attention_scores = MultiHeadAttentionBlock.attention(query , key , value , mask , self.dropout)
+
+        # combine all the heads togther
+        # (batch , h , seq_len , d_k) --> (batch , seq_len , h , d_k) --> (batch , seq_len , d_model)
+
+        x = x.transpose(1,2).contiguous().view(x.shape[0] ,-1 , self.d_k * self.h )  
+
+        # (batch , seq_len , d_model) -> (batch , seq_len , d_model)
+
+        return self.w_o(x)   
+    
+
+
+
+
+
+        
+
+    
+
+        
 
         
         
