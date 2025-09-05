0d8b90109e43eb70ce1c68204a8631d0396f66f3
shubhamZXCV <shubhamlibra2005@gmail.com>
2025-09-05 11:53:44 +0530

    Residual connection , EncoderBlock , Encoder

--- model.py
+++ model.py
@@ -140,7 +140,45 @@ class MultiHeadAttentionBlock(nn.Module):
 
         # (batch , seq_len , d_model) -> (batch , seq_len , d_model)
 
-        return self.w_o(x)   
+        return self.w_o(x)  
+
+class ResidualConnection(nn.Module):
+
+    def __init__(self , features : int , dropout : float) ->None:
+        super().__init__()
+        self.dropout = nn.Dropout()
+        self.norm  = LayerNormalisation(features)
+
+    def forward(self , x  , sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+    
+class EncoderBlock(nn.Module):
+
+    def __init__(self , features : int , self_attention_block  : MultiHeadAttentionBlock , feed_forward_block : FeedForwardBlock , dropout : float)->None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(features ,dropout) for _ in range(2)])
+
+    def forward(self , x , src_mask):
+        x = self.residual_connections[0](x , lambda x : self.self_attention_block(x,x,x,src_mask))
+        x = self.residual_connections[1](x  ,self.feed_forward_block)
+        return x
+    
+
+class Encoder(nn.Module):
+    def __init__(self  , features : int , layers : nn.ModuleList)->None:
+        super().__init__()
+        self.layers = layers
+        self.norms = LayerNormalisation(features)
+
+    def forward(self , x , mask):
+        for layer in self.layers:
+            x = layer(x,mask)
+        return self.norm(x)
+
+
+
     
 
 
